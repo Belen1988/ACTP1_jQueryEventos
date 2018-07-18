1d3fcf818d1a2ba68f150b9cb1665d85b3656f7c
Belen Aravena <aravena.belen@gmail.com>
2018-07-17 21:59:51 -0400

    21.59

--- 02/app.js
+++ 02/app.js
@@ -1,8 +1,9 @@
 $(function(){
-	$('article').eq(3).find('li>a').eq(3).text('pinterest').attr('href','http://www.pinterest.com');
-	$('[href="#"]').on('click',function(e){
-		e.preventdefault();
-	});
+$(".article").eq(3).find("li>a").eq(3).text("Pinterest").attr("href", "http://pinterest.com");
 
-	$('.header__main> h2').css('font-size','3em')	
+$('a[href="#"]').on("click", function(e){
+e.preventDefault();
 });
+
+$('.header__main > h2').css("font-size", "3em");
+})
